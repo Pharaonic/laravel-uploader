4ae0af6cd3d634496f8cb6536539bcaa7f63964d
Moamen Eltouny <16774083+MoamenEltouny@users.noreply.github.com>
2024-01-12 09:06:14 +0200

    Replace File Stream with Response Stream

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -3,15 +3,15 @@
 namespace Pharaonic\Laravel\Uploader\Controller;
 
 use App\Http\Controllers\Controller;
-use Pharaonic\Laravel\Uploader\File;
+use Illuminate\Support\Facades\Storage;
 use Pharaonic\Laravel\Uploader\Upload;
 use Pharaonic\Laravel\Uploader\UploadPermit;
 
 /**
  * Uploaded File Controller
  *
- * @version 2.0
- * @author Moamen Eltouny (Raggi) <raggi@raggitech.com>
+ * @version 3.0
+ * @author Moamen Eltouny (Raggi)
  */
 class UploadController extends Controller
 {
@@ -27,14 +27,25 @@ class UploadController extends Controller
             if (!auth()->check()) return abort(404);
 
             // If the viewer isn't the owner or uploader, check if the user has permission to view the file.
-            if ( auth()->user()->id != $file->uploader_id ) {
+            if (auth()->user()->id != $file->uploader_id) {
                 $isPermitted = UploadPermit::where('user_id', auth()->user()->id)->where('upload_id', $file->id)->first();
                 if (!$isPermitted) return abort(404);
                 if ($isPermitted->isExpired()) return abort(404);
             }
         }
 
-        // Stream the File
-        return (new File($file))->start();
+        return response()->stream(function () use ($file) {
+            if ($stream = Storage::disk($file->disk)->readStream($file->path)) {
+                while (!feof($stream)) {
+                    echo fread($stream, 1024);
+                    flush();
+                }
+                fclose($stream);
+            }
+
+            return;
+        }, 200, [
+            'Content-Type' => $file->mime
+        ]);
     }
 }
