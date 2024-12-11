ae8a84b7d181e2b18d7dfaff965dcb92adc59831
Moamen Eltouny <raggigroup@gmail.com>
2024-12-12 01:40:53 +0200

    Add route for file uploader with middleware and named action

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Pharaonic\Laravel\Uploader\Facades\Uploader;
 
-// 
+Route::middleware('web')
+    ->name('uploader.file')
+    ->get(
+        Uploader::route()->uri(),
+        Uploader::route()->action()
+    );
