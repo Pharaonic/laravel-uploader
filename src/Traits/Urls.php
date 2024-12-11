cc236e581df47b0428bdb4281bb20e5e096c2624
Moamen Eltouny <raggigroup@gmail.com>
2024-12-12 00:57:32 +0200

    Refactor Urls trait methods for clarity and functionality

--- src/Traits/Urls.php
+++ src/Traits/Urls.php
@@ -2,30 +2,23 @@
 
 namespace Pharaonic\Laravel\Uploader\Traits;
 
+use Illuminate\Support\Facades\Storage;
+use Pharaonic\Laravel\Uploader\Facades\Uploader;
+
 trait Urls
 {
     /**
-     * Getting url.
+     * Getting the file url.
      *
      * @return string
      */
-    public function url()
+    public function getUrlAttribute()
     {
-        // 
+        return $this->url();
     }
 
     /**
-     * Getting temporary url.
-     *
-     * @return string
-     */
-    public function temporaryUrl(int $expire = null)
-    {
-        // 
-    }
-
-    /**
-     * Getting temporary url.
+     * Getting the file temporary url if exists.
      *
      * @return string
      */
@@ -35,12 +28,45 @@ trait Urls
     }
 
     /**
-     * Getting url.
+     * Getting the file url.
      *
      * @return string
      */
-    public function getUrlAttribute()
+    public function url()
     {
-        return $this->url();
+        if ($this->disk == 'local') {
+            return route(
+                'uploader.file',
+                $this->hash . ((Uploader::options()['extensional'] ?? true) ? '.' . $this->extension : '')
+            );
+        }
+
+        if ($this->visibility && Storage::disk($this->disk)->providesTemporaryUrls()) {
+            return $this->temporaryUrl();
+        }
+
+        return Storage::disk($this->disk)->url($this->path);
+    }
+
+    /**
+     * Getting the file temporary url if exists.
+     *
+     * @return string
+     */
+    public function temporaryUrl(int $expire = null)
+    {
+        $driver = Storage::disk($this->disk);
+
+        if (!$driver->providesTemporaryUrls()) {
+            throw new \Exception('The driver ' . $this->disk . ' does not support temporary URLs.');
+        }
+
+        return $driver->temporaryUrl(
+            $this->path,
+            now()->addMinutes($expire ?? Uploader::options()['expire']),
+            [
+                'ResponseContentDisposition' => 'filename="' . $this->name . '"'
+            ]
+        );
     }
 }
