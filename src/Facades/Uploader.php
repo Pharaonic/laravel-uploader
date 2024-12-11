18ea8ec26cf05f90e0e1ea7f2be7526e0102deb0
Moamen Eltouny <raggigroup@gmail.com>
2024-12-12 00:47:56 +0200

    Enhance Uploader class with options retrieval, routing, and file upload functionality

--- src/Facades/Uploader.php
+++ src/Facades/Uploader.php
@@ -4,6 +4,11 @@ namespace Pharaonic\Laravel\Uploader\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static array options()
+ * @method static Router route()
+ * @method static \Pharaonic\Laravel\Uploader\Models\Upload upload(\Illuminate\Http\UploadedFile|string $file, array $options = [])
+ */
 class Uploader extends Facade
 {
     protected static function getFacadeAccessor()
--- src/Helpers.php
+++ src/Helpers.php
@@ -1,17 +1,36 @@
 <?php
 
 use Illuminate\Http\UploadedFile;
+use Pharaonic\Laravel\Uploader\Facades\Uploader;
+use Pharaonic\Laravel\Uploader\Models\Upload;
 
 if (!function_exists('upload')) {
     /**
-     * Upload a file.
+     * Upload a new file.
      *
      * @param UploadedFile|string $file
      * @param array $options
+     * 
      * @return Pharaonic\Laravel\Uploader\Upload
+     * @throws Exception if the file path does not exist.
      */
     function upload(UploadedFile|string $file, array $options = [])
     {
-        // 
+        return Uploader::upload($file, $options);
+    }
+}
+
+if (!function_exists('uploaded')) {
+    /**
+     * Get the uploaded file.
+     *
+     * @param string $hash
+     * 
+     * @return Pharaonic\Laravel\Uploader\Upload
+     * @throws Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    function uploaded(string $hash)
+    {
+        return Upload::whereHash($hash)->firstOrFail();
     }
 }
