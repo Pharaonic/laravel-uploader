3fdcba9a23adb9e92770cb93f83f82b674cd1ae9
Moamen Eltouny <raggigroup@gmail.com>
2024-12-12 00:29:11 +0200

    Add UploadObserver methods for handling updated and deleting events

--- src/Observers/UploadObserver.php
+++ src/Observers/UploadObserver.php
@@ -2,7 +2,36 @@
 
 namespace Pharaonic\Laravel\Uploader\Observers;
 
+use Illuminate\Support\Facades\Storage;
+use Pharaonic\Laravel\Uploader\Models\Upload;
+
 class UploadObserver
 {
-    // 
+    /**
+     * Handle the Upload "updated" event.
+     *
+     * @param  Upload  $upload
+     * @return void
+     */
+    public function updated(Upload $upload)
+    {
+        if ($upload->wasChanged('visibility')) {
+            Storage::disk($upload->disk)->setVisibility($upload->path, $upload->visibility);
+        }
+    }
+
+    /**
+     * Handle the Upload "deleting" event.
+     *
+     * @param  Upload  $file
+     * @return void
+     */
+    public function deleting(Upload $file)
+    {
+        if ($file->thumbnail_id) {
+            $file->thumbnail->delete();
+        }
+
+        // todo: delete action
+    }
 }
